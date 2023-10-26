daacf1d55501bef7d9a4b5a06e2e70e4ac604f37
Nikitos <ngladkikh@adsterra.com>
2023-10-26 22:44:11 +0500

    chore: change theme palettes

---
+++ plugins/astrotheme.lua
@@ -0,0 +1,13 @@
+return {
+  "AstroNvim/astrotheme",
+  opts = {
+    palettes = {
+      astrodark = {
+        ui = {
+          selection = "#374b5c",
+          menu_selection = "#26343F"
+        },
+      },
+    },
+  }
+}
