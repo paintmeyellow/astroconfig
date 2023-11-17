76e3092f4e269977529d5658a00381c34a409048
Nikitos <ngladkikh@adsterra.com>
2023-11-17 22:42:30 +0500

    chore(plugins): disable better-escape

--- plugins/core.lua
+++ plugins/core.lua
@@ -49,7 +49,7 @@ return {
     end,
   },
   -- You can disable default plugins as follows:
-  -- { "max397574/better-escape.nvim", enabled = false },
+  { "max397574/better-escape.nvim", enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
